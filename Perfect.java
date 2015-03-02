c4cc84df3e491e2f811c7f697fcd129b6d9d3d99
Nathaniel D. Smith <smithnd13@uww.edu>
2015-03-02 16:18:31 -0600

    Create Perfect.java

---
+++ Perfect.java
@@ -0,0 +1,33 @@
+//Nathaniel Smith
+//2-27-13
+//Exercise #2  prob 1 ch 5
+
+class Perfect{
+	public static void main(String[] args) {
+
+//print all perfect numbers b/w 1 and 1000
+
+	for (int i = 1; i<1000; i++){
+	if (Perfect(i) == true)
+	System.out.print(i + "  ");}
+
+
+	}//end main()
+
+//define a method that checks if an integer is a perfect number
+public static boolean Perfect(int x) {
+	int sum = 0;
+
+	for (int i = 1; i<x; i++){
+	if (x % i ==0)
+		sum +=i;} //end for
+
+		if (sum == x)
+		return true;
+		else
+		return false;
+
+}//end perfect()
+
+
+}//end class
