22b131d61805ad523866d4146cf96c678b6cc8fc
Nathaniel D. Smith <smithnd13@uww.edu>
2015-03-02 15:59:11 -0600

    Create Loops.java

---
+++ Loops.java
@@ -0,0 +1,21 @@
+//Nathaniel D. Smith 
+//2-13-13
+//Program loops
+
+import javax.swing.JOptionPane;
+import java.text.DecimalFormat;
+//import java.lang.Math;//default for any java program
+
+class Loops {
+
+	public  static void main(String[] args){
+
+	//for (int i = 1; i<=20; i++)
+	//for (int i = 2; i<=20; i = i+2)
+	for (int i = 2; i<=20; i+=2)
+	System.out.println(i + "  ");
+
+
+
+	}
+}
